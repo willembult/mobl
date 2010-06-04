2d507362fedb967dd12ca03524366fe85169405b
zef <zef@tud11962.tudelft.net>
2010-06-04 15:07:00 +0200

    Wizard, find standard library in plug-in directory.

---
+++ editor/java/mobl/MoblEditorWizard.java
@@ -0,0 +1,250 @@
+package mobl;
+
+import static org.eclipse.core.resources.IResource.DEPTH_INFINITE;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.strategoxt.imp.runtime.EditorState;
+import org.strategoxt.imp.runtime.Environment;
+
+/**
+ * A wizard for creating new WebDSL projects.
+ */
+public class MoblEditorWizard extends Wizard implements INewWizard {
+
+    private final MoblEditorWizardPage input = new MoblEditorWizardPage();
+
+    private IProject lastProject;
+
+    // TODO: Support external directory and working set selection in wizard
+
+    public MoblEditorWizard() {
+        setNeedsProgressMonitor(true);
+    }
+
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        // No further initialization required
+    }
+
+    @Override
+    public void addPages() {
+        addPage(input);
+    }
+
+    /**
+     * This method is called when 'Finish' button is pressed in the wizard. We
+     * will create an operation and run it using wizard as execution context.
+     */
+    @Override
+    public boolean performFinish() {
+        final String appName = input.getInputAppName();
+        final String projectName = input.getInputProjectName();
+        System.out.println(appName + projectName);
+
+        IRunnableWithProgress op = new IRunnableWithProgress() {
+            public void run(IProgressMonitor monitor) throws InvocationTargetException {
+                try {
+                    doFinish(appName, projectName, monitor);
+                } catch (Exception e) {
+                    throw new InvocationTargetException(e);
+                } finally {
+                    monitor.done();
+                }
+            }
+        };
+
+        try {
+            getContainer().run(true, false, op);
+        } catch (InterruptedException e) {
+            rollback();
+            return false;
+        } catch (InvocationTargetException e) {
+            Throwable realException = e.getTargetException();
+            Environment.logException("Exception while creating new project", realException);
+            MessageDialog.openError(getShell(), "Error: " + realException.getClass().getName(), realException
+                    .getMessage());
+            rollback();
+            return false;
+        }
+        return true;
+    }
+
+    private void rollback() {
+        // monitor.setTaskName("Undoing workspace operations");
+        try {
+            if (lastProject != null)
+                lastProject.delete(true, null);
+        } catch (CoreException e) {
+            Environment.logException("Could not remove new project", e);
+        }
+    }
+
+    private void doFinish(String appName, String projectName, IProgressMonitor monitor) throws IOException,
+            CoreException {
+        final int TASK_COUNT = 3;
+        lastProject = null;
+        monitor.beginTask("Creating " + appName + " application", TASK_COUNT);
+
+        monitor.setTaskName("Creating Eclipse project");
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IProject project = lastProject = workspace.getRoot().getProject(projectName);
+        project.create(null);
+        project.open(null);
+        monitor.worked(1);
+
+        // agent.setWorkingDir(project.getLocation().toOSString());
+        String plugindir = mobl.MoblEditorWizard.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+        if (Platform.getOS().equals(Platform.OS_WIN32)) { // FIXME: proper paths
+            // on Windows
+            plugindir = plugindir.substring(1);
+        }
+        /*
+         * if (!jar1.endsWith(".jar")) { // ensure correct jar at development
+         * time String jar1a = jar1 + "/../strategoxt.jar"; if (new
+         * File(jar1a).exists()) jar1 = jar1a; jar1a = jar1 +
+         * "/java/strategoxt.jar"; if (new File(jar1a).exists()) jar1 = jar1a; }
+         */
+        System.out.println("path: " + plugindir);
+
+        try {
+            monitor.setTaskName("Copying runtime");
+            
+        	String wwwDir = project.getLocation().toString() + "/www/lib";
+        	createDirs(wwwDir);
+            copyDirectory(new File(plugindir+"www/lib"), new File(wwwDir));
+            
+            monitor.setTaskName("Generating default app");
+            StringBuilder sb = new StringBuilder();
+            sb.append("application " + appName + "\n");
+            sb.append("\n");
+            sb.append("import mobl\n");
+            sb.append("\n");
+            sb.append("screen root() {\n");
+            sb.append("\theader(\"" + appName + "\")\n");
+            sb.append("}\n");
+            monitor.worked(1);
+            writeStringToFile(sb.toString(), project.getLocation().toString()+"/"+appName + ".mobl");
+        } catch (IOException e) {
+            Environment.logException(e);
+            throw e;
+        }
+
+        monitor.worked(1);
+        refreshProject(project);
+
+        monitor.setTaskName("Opening editor tab");
+
+        Display display = getShell().getDisplay();
+        EditorState.asyncOpenEditor(display, project.getFile(appName + ".mobl"), true);
+        monitor.worked(1);
+    }
+
+    public static void writeStringToFile(String s, String file) throws IOException {
+        FileOutputStream in = null;
+        try {
+            File buildxml = new File(file);
+            in = new FileOutputStream(buildxml);
+            FileChannel fchan = in.getChannel();
+            BufferedWriter bf = new BufferedWriter(Channels.newWriter(fchan, "UTF-8"));
+            bf.write(s);
+            bf.close();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public static void createDirs(String dirs) {
+        new File(dirs).mkdirs();
+    }
+
+    public static void copyFile(String ssource, String sdest) throws IOException {
+        System.out.println("Copying " + ssource + " to " + sdest);
+        File dest = new File(sdest);
+        File source = new File(ssource);
+        if (!dest.exists()) {
+            dest.createNewFile();
+        }
+        FileChannel in = null;
+        FileChannel out = null;
+        try {
+            in = new FileInputStream(source).getChannel();
+            out = new FileOutputStream(dest).getChannel();
+            out.transferFrom(in, 0, in.size());
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    // If targetLocation does not exist, it will be created.
+    public void copyDirectory(File sourceLocation, File targetLocation) throws IOException {
+
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i = 0; i < children.length; i++) {
+                copyDirectory(new File(sourceLocation, children[i]), new File(targetLocation, children[i]));
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+    }
+
+    private void refreshProject(final IProject project) {
+        try {
+            NullProgressMonitor monitor = new NullProgressMonitor();
+            project.refreshLocal(DEPTH_INFINITE, monitor);
+            project.close(monitor);
+            project.open(monitor);
+
+        } catch (CoreException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ editor/java/mobl/MoblEditorWizardPage.java
@@ -0,0 +1,163 @@
+package mobl;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.imp.preferences.fields.RadioGroupFieldEditor;
+import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * The "New" wizard page allows setting the container for the new file as well
+ * as the file name. The page will only accept file name without the extension
+ * OR with the extension that matches the expected one (esv).
+ */
+
+public class MoblEditorWizardPage extends WizardPage {
+
+    private Text inputProjectName;
+
+    public String getInputProjectName() {
+        return inputProjectName.getText().trim();
+    }
+
+    private Text inputAppName;
+
+    public String getInputAppName() {
+        return inputAppName.getText().trim();
+    }
+
+    private boolean isInputProjectNameChanged;
+
+    private boolean ignoreEvents;
+
+    /**
+     * Constructor for SampleNewWizardPage.
+     */
+    public MoblEditorWizardPage() {
+        super("wizardPage");
+        setTitle("mobl project");
+        setDescription("This wizard creates a mobl project.");
+    }
+
+    /**
+     * @see IDialogPage#createControl(Composite)
+     */
+    public void createControl(Composite parent) {
+        Composite container = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        container.setLayout(layout);
+        layout.numColumns = 2;
+        layout.verticalSpacing = 9;
+
+        new Label(container, SWT.NULL).setText("&Project name:");
+        inputProjectName = new Text(container, SWT.BORDER | SWT.SINGLE);
+        inputProjectName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        inputProjectName.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                if (!ignoreEvents) {
+                    distributeProjectName();
+                    onChange();
+                }
+            }
+        });
+
+        new Label(container, SWT.NULL).setText("&Application name:");
+        inputAppName = new Text(container, SWT.BORDER | SWT.SINGLE);
+        inputAppName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        inputAppName.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                onChange();
+            }
+        });
+
+        setControl(container);
+        setPageComplete(false);
+
+        inputProjectName.setFocus();
+    }
+
+    private void distributeProjectName() {
+        if (!isInputProjectNameChanged || getInputAppName().length() == 0
+                || getInputAppName().equals(toLanguageName(getInputProjectName()))) {
+            ignoreEvents = true;
+            inputAppName.setText(toLanguageName(getInputProjectName()));
+            isInputProjectNameChanged = false;
+            ignoreEvents = false;
+        }
+    }
+
+    /**
+     * Ensures that both text fields are set.
+     */
+    private void onChange() {
+        if (!ignoreEvents) {
+            setErrorMessage(null);
+
+            if (getInputProjectName().length() == 0) {
+                setErrorStatus("Project name must be specified");
+                return;
+            }
+            if (getInputAppName().length() == 0) {
+                setErrorStatus("Application name must be specified");
+                return;
+            }
+
+            if (!isValidProjectName(getInputProjectName())) {
+                setErrorStatus("Project name must be valid");
+                return;
+            }
+            if (!toLanguageName(getInputAppName()).equalsIgnoreCase(getInputAppName())) {
+                setErrorStatus("Application name must be valid");
+                return;
+            }
+
+            IWorkspace workspace = ResourcesPlugin.getWorkspace();
+            if (workspace.getRoot().getProject(getInputProjectName()).exists()) {
+                setErrorStatus("A project with this name already exists");
+                return;
+            }
+
+            if (getInputProjectName().indexOf(' ') != -1) {
+                setWarningStatus("Project names with spaces may not be supported depending on your configuration");
+            } else {
+                setErrorStatus(null);
+            }
+        }
+    }
+
+    private static boolean isValidProjectName(String name) {
+        for (char c : name.toCharArray()) {
+            if (!(Character.isLetterOrDigit(c) || c == '_' || c == ' ' || c == '-' || c == '.' || c == '(' || c == ')'
+                    || c == '#' || c == '+' || c == '[' || c == ']' || c == '@'))
+                return false;
+        }
+        return true;
+    }
+
+    private static String toLanguageName(String name) {
+        return name;
+    }
+
+    private void setErrorStatus(String message) {
+        setErrorMessage(message);
+        setPageComplete(message == null);
+    }
+
+    private void setWarningStatus(String message) {
+        if (getErrorMessage() == null)
+            setErrorMessage(message);
+    }
+
+}
