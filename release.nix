a41ee5e85b88a263660635e9e393813d0cea1af7
Zef Hemel <zef@zef.me>
2010-10-22 15:36:13 +0200

    First version of jQuery mobile control set (not complete, nor bug-free)

--- release.nix
+++ release.nix
@@ -108,6 +108,7 @@ let
 
     samples = {
       controldemo        = moblc { name = "control-demo"; app = "demo.mobl"; } ;
+      controldemo_jquery = moblc { name = "control-demo"; app = "jqdemo.mobl"; } ;
       draw               = moblc { name = "draw"; app = "draw.mobl"; } ;
       geo                = moblc { name = "geo"; app = "maptest.mobl"; } ;
       helloserver_client = moblc { name = "helloserver"; app = "client.mobl"; } ;
