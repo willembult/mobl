ad5d28e67af00d541ee7abfb14126bb88a94d960
Zef Hemel <zef@zef.me>
2011-02-14 11:09:34 +0100

    Remove draw

--- release.nix
+++ release.nix
@@ -111,7 +111,7 @@ let
     samples = {
       controldemo        = moblc { name = "control-demo"; app = "demo.mobl"; } ;
       controldemo_jquery = moblc { name = "control-demo"; app = "jqdemo.mobl"; } ;
-      draw               = moblc { name = "draw"; app = "draw.mobl"; } ;
+      //draw               = moblc { name = "draw"; app = "draw.mobl"; } ;
       geo                = moblc { name = "geo"; app = "maptest.mobl"; } ;
       helloserver_client = moblc { name = "helloserver"; app = "client.mobl"; } ;
       #helloserver_server = moblc { name = "helloserver"; app = "server.mobl"; stdlib = "${mobl}/stdlib-server-override"; } ;
