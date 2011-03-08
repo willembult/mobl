fd4e2c8dbe8099b69864e2dffe5785641b3bc93a
Zef Hemel <zef@zef.me>
2011-03-08 09:32:41 +0100

    Build updates

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ if [ ! -d utils ]; then
   curl -O http://zef.me/mobl/make_permissive.jar
   curl -O http://zef.me/mobl/sdf2imp.jar
   curl -O http://zef.me/mobl/strategoxt.jar
+  curl -O http://zef.me/mobl/ecj.jar
   cd ..
 fi
-ANT_OPTS="-Declipse.spoofaximp.jars=utils/ -ss16m -Xmx1024m" LOCALCLASSPATH=utils/aster.jar:utils/make_permissive.jar:utils/sdf2imp.jar:utils/strategoxt.jar ant -f build.main.xml moblc
+#ANT_OPTS="-Declipse.spoofaximp.jars=utils/ -ss16m -Xmx1024m" LOCALCLASSPATH=utils/aster.jar:utils/make_permissive.jar:utils/sdf2imp.jar:utils/strategoxt.jar:utils/ecj.jar ant -f build.main.xml moblc
+ANT_OPTS="-Declipse.spoofaximp.jars=utils/ -ss16m -Xmx1024m" ant -lib utils -f build.main.xml moblc
